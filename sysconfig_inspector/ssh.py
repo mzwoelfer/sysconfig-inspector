61df61f4840d0f1ad346a410074275a9fab0cc85
mzwoelfer <>
2025-06-09 21:27:20 +0200

    Find sshd config paths

--- sysconfig_inspector/ssh.py
+++ sysconfig_inspector/ssh.py
@@ -1,3 +1,29 @@
+import os
+from typing import List, Optional
+
+
 class SSHInspector():
-    def __init__(self):
-        return
+    SSHD_CONFIG_PATH = '/etc/ssh/sshd_config'
+    SSH_CONFIG_PATH = '/etc/ssh/ssh_config'
+
+    def __init__(self, 
+                 sshd_config_path: Optional[str] = None):
+        self._sshd_config_path = sshd_config_path if sshd_config_path is not None else self.SSHD_CONFIG_PATH
+        self._config_file_paths: List[str] = []
+        self._discover_config_files()
+
+    @property
+    def config_file_paths(self) -> List[str]:
+        return self._config_file_paths
+
+
+    def _discover_config_files(self) -> None:
+        """
+        Discovers SSH configurations files.
+        """
+        found_files: List[str] = []
+
+        if os.path.isfile(self._sshd_config_path):
+            found_files.append(self._sshd_config_path)
+
+        self._config_file_paths = found_files
