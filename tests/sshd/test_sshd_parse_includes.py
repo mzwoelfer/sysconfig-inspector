5f5c6b18c775009bcf93c6fd2655a98ccec1d55e
mzwoelfer <>
2025-06-18 09:05:34 +0200

    Remove tests for includes. Only reads as key-value

--- tests/sshd/test_sshd_parse_includes.py
+++ tests/sshd/test_sshd_parse_includes.py
@@ -5,12 +5,11 @@ from tests.sshd.test_sshd import BaseSshInspectorTest
 
 print(f"DEBUG_TEST: Effective User ID (euid): {os.geteuid()}")
 # --- TEST INCLUDES FUNCTIONALITY ---
-@unittest.skip("Skipping includes functionality... for now")
 class TestIncludesFunctionality(BaseSshInspectorTest):
-    def test_includes_configuration_correctly(self):
+    def test_does_not_include_configuration(self):
         """
-        Include /etc/ssh/sshd_config.d/*.conf
-        First items mentioned take precedence
+        Do not include /etc/ssh/sshd_config.d/*.conf
+        Just read a normal key-value
         """
         main_config_content = f"""
             Include {self.included_sshd_dir_path}/*.conf
@@ -31,56 +30,11 @@ class TestIncludesFunctionality(BaseSshInspectorTest):
 
         expected_output = {
             "Include": f"{self.included_sshd_dir_path}/*.conf",
-            "PubkeyAuthentication" : False,
+            "PubkeyAuthentication" : True,
         }
 
         sshd_inspector = SSHDInspector(
             sshd_config_path=sshd_config
-        )
-
-        self.assertEqual(sshd_inspector.sshd_config, expected_output)
-
-    def test_includes_match_block_correctly(self):
-        """
-        Parse included match block and add to sshd config
-        """
-        included_match_content = """
-            Match User testuser
-                PermitRootLogin no
-                PasswordAuthentication yes
-        """
-        included_file_path = self.create_test_file(
-            '/etc/ssh/sshd_config.d/50-match-user.conf', 
-            contents=included_match_content
-        )
-
-        main_config_content = f"""
-            Port 22
-            Include {self.included_sshd_dir_path}/*.conf 
-            HostKey /etc/ssh/ssh_host_rsa_key
-        """
-        main_sshd_config_path = self.create_test_file(
-            '/etc/ssh/sshd_config',
-            contents=main_config_content
-        )
-
-
-        expected_output = {
-            "Port": 22,
-            "HostKey": "/etc/ssh/ssh_host_rsa_key",
-            "Include": f"{self.included_sshd_dir_path}/*.conf", 
-            "Match": [
-                {
-                    "User testuser": {
-                        "PermitRootLogin": False,
-                        "PasswordAuthentication": True
-                    }
-                }
-            ]
-        }
-
-        sshd_inspector = SSHDInspector(
-            sshd_config_path=main_sshd_config_path
         )
 
         self.assertEqual(sshd_inspector.sshd_config, expected_output)
